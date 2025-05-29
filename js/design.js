5c359d884ba75720094dbdc403fa71a099f92512
Johanna <jolu0002@stud.kea.dk>
2025-05-29 12:01:14 +0200

    laver burgermenu, og opretter forside og 'om mig'

--- js/design.js
+++ js/design.js
@@ -2,16 +2,20 @@
 
 const burger = document.querySelector(".burger");
 const nav = document.querySelector("nav");
-const menu = document.querySelector(".menu");
+const menu = document.querySelector(".burger.active");
+const overlay = document.querySelector(".blurry");
 
 burger.addEventListener("click", burgerClick);
 function burgerClick() {
   burger.classList.toggle("active");
   nav.classList.toggle("active");
+  overlay.classList.toggle("body-blur");
 }
+
 menu.addEventListener("click", menuClick);
 function menuClick() {
   burger.classList.remove("active");
   nav.classList.remove("active");
 }
+
 
